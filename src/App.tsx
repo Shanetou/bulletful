044539d8c9c14f2e17672985be9c32e9d375f8ad
Shane McQuerter <shanemcquerter@gmail.com>
2020-04-10 17:36:13 -0700

    Make some convenience hooks and functions for the firebase service

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,30 @@ import {
   useRealtimeCollection,
   addDocToCollection,
   Collection,
+  useGetCollection,
+  deleteHalfOfAllBullets,
 } from "./firebase/service";
 
 const bulletsFromFactory = bulletFactory(10);
 console.log("bulletsFromFactory:", bulletsFromFactory);
 
-bulletsFromFactory.forEach((bullet) => {
-  addDocToCollection(Collection.bullets, bullet);
-});
+// bulletsFromFactory.forEach((bullet) => {
+//   addDocToCollection(Collection.bullets, bullet);
+// });
 
 function App() {
-  const bullets = useRealtimeCollection(Collection.bullets);
+  // const bullets = useRealtimeCollection(Collection.bullets);
+  // const bullets = useRealtimeCollection(Collection.bullets);
+  const [bullets, getCollection] = useGetCollection(Collection.bullets);
   console.log("bullets:", bullets);
 
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => getCollection()}>Get Collection</button>
+        <button onClick={() => deleteHalfOfAllBullets(bullets)}>
+          Delete half of bullets
+        </button>
         {/* <Bullets bullets={bulletFactory(12)} /> */}
       </header>
     </div>
--- src/firebase/service.ts
+++ src/firebase/service.ts
@@ -5,6 +5,21 @@ export enum Collection {
   bullets = "bullets",
 }
 
+interface Entity {
+  id: string;
+}
+
+const dataFromSnapshot = <T extends Entity>(
+  snapshot: firebase.firestore.DocumentSnapshot
+): T | undefined => {
+  if (!snapshot.exists) return undefined;
+  const data = snapshot.data() as any;
+  return {
+    ...data,
+    id: snapshot.id,
+  };
+};
+
 // Add loading finite state machine
 export function useRealtimeCollection(collection: Collection) {
   const [bullets, setBullets] = React.useState([]);
@@ -16,7 +31,7 @@ export function useRealtimeCollection(collection: Collection) {
 
       snapshot.forEach(
         (doc) => {
-          bullets.push(doc.data());
+          bullets.push(dataFromSnapshot(doc));
         },
         // @ts-ignore
         (error) => {
@@ -34,14 +49,63 @@ export function useRealtimeCollection(collection: Collection) {
   return bullets;
 }
 
+// Add loading finite state machine
+export function useGetCollection(collection: Collection): [any[], () => void] {
+  const [bullets, setBullets] = React.useState([]);
+
+  const getCollection = () => {
+    db.collection(collection).onSnapshot((snapshot) => {
+      // @ts-ignore
+      const bullets = [];
+
+      snapshot.forEach(
+        (doc) => {
+          bullets.push(dataFromSnapshot(doc));
+        },
+        // @ts-ignore
+        (error) => {
+          console.log("error:", error);
+        }
+      );
+
+      // @ts-ignore
+      setBullets(bullets);
+    });
+  };
+
+  return [bullets, getCollection];
+}
+
 // @ts-ignore
 export function addDocToCollection(collection: Collection, doc) {
   db.collection(collection)
     .add(doc)
-    .then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
+    .then((docRef) => {
+      console.log(`Document ${docRef.id} written to ${collection}`, docRef.id);
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.error("Error adding document: ", error);
     });
+}
+
+export function deleteDocFromCollection(collection: string, docId: string) {
+  db.collection(collection)
+    .doc(docId)
+    .delete()
+    .then(() => {
+      console.log(`Document ${docId} deleted from ${collection}!`);
+    })
+    .catch((error) => {
+      console.error("Error removing document: ", error);
+    });
+}
+
+export function deleteHalfOfAllBullets<T extends Entity>(bullets: T[]): void {
+  let count = bullets.length;
+  let countToDelete = Math.floor(count / 2);
+  let docsToDelete = bullets.slice(countToDelete, count);
+
+  docsToDelete.forEach((doc) => {
+    deleteDocFromCollection(Collection.bullets, doc.id);
+  });
 }
