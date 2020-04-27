fce087040c7f09220a9dd098f30692d1e11cf932
Shane McQuerter <shanemcquerter@gmail.com>
2020-04-26 22:23:59 -0700

    Attempt to add offline persistence; get TS errors

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -23,5 +23,17 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
+// .enablePersistence()
+// .catch(function (err) {
+//   if (err.code == "failed-precondition") {
+//     // Multiple tabs open, persistence can only be enabled
+//     // in one tab at a a time.
+//     // ...
+//   } else if (err.code == "unimplemented") {
+//     // The current browser does not support all of the
+//     // features required to enable persistence
+//     // ...
+//   }
+// });
 
 export { db };
